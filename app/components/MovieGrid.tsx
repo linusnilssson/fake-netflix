e1cced8f043d71bb55c67d23155244c75645944f
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-03-25 16:39:22 +0100

    Added All movies-title above movie-grid

--- app/components/MovieGrid.tsx
+++ app/components/MovieGrid.tsx
@@ -1,29 +1,32 @@
 "use client";
-import { Box, Card, CardMedia } from "@mui/material";
+import { Box, Card, CardMedia, Typography } from "@mui/material";
 import movies from "../../data/movies.json";
 
 export default function MovieGrid() {
   return (
-    <Box
-      sx={{
-        backgroundColor: "#000000",
-        display: "grid",
-        gap: 2,
-        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-        p: 2,
-      }}
-    >
-      {movies.map((movie, index) => (
-        <Card key={index} sx={{ display: "flex", flexDirection: "column" }}>
-          <CardMedia
-            component="img"
-            src={movie.thumbnail}
-            alt={movie.title}
-            loading="lazy"
-            sx={{ height: 300, objectFit: "cover" }}
-          />
-        </Card>
-      ))}
+    <Box sx={{ backgroundColor: "#000000", p: 2 }}>
+      <Typography variant="h3" sx={{ color: "white", mb: 2 }}>
+        All movies
+      </Typography>
+      <Box
+        sx={{
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+        }}
+      >
+        {movies.map((movie, index) => (
+          <Card key={index} sx={{ display: "flex", flexDirection: "column" }}>
+            <CardMedia
+              component="img"
+              src={movie.thumbnail}
+              alt={movie.title}
+              loading="lazy"
+              sx={{ height: 300, objectFit: "cover" }}
+            />
+          </Card>
+        ))}
+      </Box>
     </Box>
   );
 }
