bf92a5b2e39db9e9eb6b702956b3939768dbe683
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-03-26 10:01:23 +0100

    Added grid for movies as a quick mockup. In progress... this will later be connected to clicks and "added" to bookmark

--- app/bookmark/page.tsx
+++ app/bookmark/page.tsx
@@ -1,7 +1,31 @@
-export default function BookmarkPage () {
-    return (
-        <div>
-            <h1>Bookmark Page</h1>
-        </div>
-    )
+import { Box, Card, CardMedia, Typography } from "@mui/material";
+import movies from "../../data/movies.json";
+
+export default function BookmarkPage() {
+  return (
+    <Box sx={{ backgroundColor: "#000000", p: 2 }}>
+      <Typography variant="h4" sx={{ color: "white" }}>
+        Bookmark Page
+      </Typography>
+      <Box
+        sx={{
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
+        }}
+      >
+        {movies.map((movie, index) => (
+          <Card key={index} sx={{ display: "flex", flexDirection: "column" }}>
+            <CardMedia
+              component="img"
+              src={movie.thumbnail}
+              alt={movie.title}
+              loading="lazy"
+              sx={{ height: 200, objectFit: "cover" }}
+            />
+          </Card>
+        ))}
+      </Box>
+    </Box>
+  );
 }
